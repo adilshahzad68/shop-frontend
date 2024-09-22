646cc1289be428decac46046ff10c96919a18732
Adil <adilshahzad684@gmail.com>
2024-09-22 15:33:36 +0200

    added blog

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -5,17 +5,64 @@ const Blog = () => {
 
     return (
         <div className='py-24 pb-56 w-5/6 mx-auto'>
-            <div className='text-center'>
+            <div className='text-left'>
                 <div className="card bg-base-100 shadow-xl">
                     <div className="card-body">
-                        <h2 className="text-center font-bold text-xl"> Developed by Adil Shahzad</h2>
-                        <div>
-                            <p>Contact</p>
+                        <h2 className="text-center font-bold text-xl"> Welcome to Buy and Sell Store, Online Marketplace</h2>
+                        
+                        
+        <p>Buy and Sell Store is an innovative online platform where users can buy and sell a wide range of new and used products. Whether you are looking to declutter your home, find a great deal, or make some extra cash by selling your items, Buy and Sell Store has got you covered!</p>
+
+        <p>With a user-friendly interface and secure online payment options, we make buying and selling products easier.</p>  <br></br>
+
+        <h2 className="text-left font-bold text-xl">Key Features of Buy and Sell Store:</h2> 
+        <ul>
+        
+            <li><strong>Online Buying & Selling: </strong>
+            <p> At Buy and Sell Store, users can seamlessly buy or sell products such as electronics, clothing, home appliances, and more. Whether you are selling old gadgets or hunting for great products, our platform connects buyers and sellers from all over. </p></li>
+
+            <li><strong> New & Used Products: </strong>
+            <p>We offer a diverse marketplace where both new and second-hand products are available. Sellers can post detailed listings of their items, including photos, descriptions, prices, and conditions.</p></li>
+        
+
+        <li><strong> Easy Product Listings for Sellers: </strong>
+        <p>As a seller, you can add products with all the necessary details, including product images, descriptions, pricing, and condition status new or used. Our platform allows sellers to manage their listings efficiently and reach a wide audience.</p>
+        </li>
+        
+
+        <li><strong>Admin Management:</strong>
+        <p>Our application also includes an admin panel where administrators can manage buyers and sellers. The admin can approve or delete sellers, monitor to ensure a smooth and secure user experience for all.</p></li>
+
+        <li><strong>Secure Online Payments:</strong>
+        <p>For added convenience and security, Buy and Sell Store integrates with secure online payment method. Buyers can purchase products directly through the platform with peace of mind, knowing that their transactions are safe and secure.</p></li>
+
+        <li><strong>Reports and Analytics:</strong>
+        <p>The admin panel features comprehensive, offering insights into the platform's activities. Administrators can evaluate the overall performance of the platform.</p></li>
+        <br></br>
+
+       </ul>
+        
+        <h2 className="text-left font-bold text-xl">Why Choose Buy and Sell Store?</h2>
+
+        <ul>
+            <li><strong>Convenience:</strong> Browse and shop from anywhere, at any time, from your phone or computer.</li>
+            <li><strong>Trust & Security:</strong> We prioritize the safety of our users by implementing robust security features, including secure payments and user verification.</li>
+            <li><strong>Wide Selection:</strong> Choose from a variety of new and pre-loved items across multiple categories.</li>
+            <li><strong>Community Focused:</strong> Connect with other buyers and sellers.</li>
+        </ul>
+
+        <p>Whether you're selling your old tech or searching for that perfect second-hand treasure, Buy and Sell Store is the perfect place to connect buyers and sellers. Join us today and experience a smarter way to shop and sell online!</p>
+    
+        <br></br>  
+                  
+                        <div className="text-center font-10px ">
+                           
+                            <p>Contact Developer</p>
                             <p> Email: adilshahzad684@gmail.com <br />
 
                                 Mobile: +48-576227792 <br />
 
-                                Thanks for visiting my Application:)</p>
+                                Thanks for visiting </p>
                         </div>
 
                     </div>
